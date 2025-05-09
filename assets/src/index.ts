c1ea5f77ada130ecd8b876e2baa712bb257b00f5
Behnam Tehrani <behnam777@gmail.com>
2025-05-09 10:36:00 -0400

    removed assets

--- assets/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AiAssistant } from './AiAssistant';
-export type { AiAssistantProps } from './types';
-
-// Also export the styles in case users want to customize them
-export { default as styles } from './styles.module.css'; 
